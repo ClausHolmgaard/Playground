24ed6dc81f8d367d227dec734a9052bec449629a
ClausHolmgaard <github@clausnet.dk>
2018-12-19 15:26:19 +0100

    sync

--- MachineLearning/SqueezeDet/SqueezeDetHelpers.py
+++ MachineLearning/SqueezeDet/SqueezeDetHelpers.py
@@ -3,6 +3,8 @@
 from keras.layers import Input, Conv2D, concatenate
 from keras.initializers import TruncatedNormal
 from keras.regularizers import l2
+from keras import backend as K
+import numpy as np
 
 
 def fire_layer(name, input, s1x1, e1x1, e3x3, weight_decay, stdd=0.01):
@@ -19,19 +21,39 @@ def fire_layer(name, input, s1x1, e1x1, e3x3, weight_decay, stdd=0.01):
     """
 
     sq1x1 = Conv2D(
-        name = name + '/squeeze1x1', filters=s1x1, kernel_size=(1, 1), strides=(1, 1), use_bias=True,
-        padding='SAME', kernel_initializer=TruncatedNormal(stddev=stdd), activation="relu",
-        kernel_regularizer=l2(weight_decay))(input)
+        name = name + '/squeeze1x1',
+        filters=s1x1,
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        use_bias=True,
+        padding='SAME',
+        kernel_initializer=TruncatedNormal(stddev=stdd),
+        activation="relu",
+        #kernel_regularizer=l2(weight_decay)
+        )(input)
 
     ex1x1 = Conv2D(
-        name = name + '/expand1x1', filters=e1x1, kernel_size=(1, 1), strides=(1, 1), use_bias=True,
-        padding='SAME',  kernel_initializer=TruncatedNormal(stddev=stdd), activation="relu",
-        kernel_regularizer=l2(weight_decay))(sq1x1)
+        name = name + '/expand1x1',
+        filters=e1x1,
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        use_bias=True,
+        padding='SAME',
+        kernel_initializer=TruncatedNormal(stddev=stdd),
+        activation="relu",
+        #kernel_regularizer=l2(weight_decay)
+        )(sq1x1)
 
     ex3x3 = Conv2D(
-        name = name + '/expand3x3',  filters=e3x3, kernel_size=(3, 3), strides=(1, 1), use_bias=True,
-        padding='SAME', kernel_initializer=TruncatedNormal(stddev=stdd), activation="relu",
-        kernel_regularizer=l2(weight_decay))(sq1x1)
+        name = name + '/expand3x3',
+        filters=e3x3, kernel_size=(3, 3),
+        strides=(1, 1),
+        use_bias=True,
+        padding='SAME',
+        kernel_initializer=TruncatedNormal(stddev=stdd),
+        activation="relu",
+        #kernel_regularizer=l2(weight_decay)
+        )(sq1x1)
 
     return concatenate([ex1x1, ex3x3], axis=3)
 
@@ -40,3 +62,9 @@ if __name__ == "__main__":
     input_layer = Input(shape=(200, 200, 3), name="input")
 
     fire = fire_layer(name="fire", input = input_layer, s1x1=16, e1x1=64, e3x3=64, weight_decay=0.001)
+
+def binary_crossentropy(y, y_hat, epsilon):
+    return y * (-np.log(y_hat + epsilon)) + (1-y) * (-np.log(1-y_hat + epsilon))
+
+def keras_binary_crossentropy(y, y_hat, epsilon):
+    return y * (-K.log(y_hat + epsilon)) + (1-y) * (-K.log(1-y_hat + epsilon))
