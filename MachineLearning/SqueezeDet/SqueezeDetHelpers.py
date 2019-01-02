26a900fdb2a861fc373253bb4d0ada1904c29465
ClausHolmgaard <github@clausnet.dk>
2019-01-02 19:54:35 +0100

    sync

--- MachineLearning/SqueezeDet/SqueezeDetHelpers.py
+++ MachineLearning/SqueezeDet/SqueezeDetHelpers.py
@@ -73,6 +73,30 @@ def binary_crossentropy(y, y_hat, epsilon):
 def keras_binary_crossentropy(y, y_hat, epsilon):
     return y * (-K.log(y_hat + epsilon)) + (1-y) * (-K.log(1-y_hat + epsilon))
 
+def get_all_points_from_prediction(pred, anchors, threshold=1.0, do_scale=True):
+    """
+    pred is a prediction map in the shape (ANCHOR_HEIGHT, ANCHOR_WIDTH, 3)
+    """
+    # Get all points with a confidence above threshold
+    label_indicies = np.where(pred[:, :, 0] >= threshold)
+    num_points = len(label_indicies[0])
+    points = np.zeros((num_points, 4))
+    
+    # Loop through all anchor points
+    for c, (x_anchor, y_anchor) in enumerate(zip(label_indicies[0], label_indicies[1])):
+        # when anchor location is known, the location of the closest anchor in the actual image can be found
+        x_without_offset, y_without_offset = anchors[x_anchor, y_anchor]
+        
+        # The offset can then be extracted from the labels
+        (x_offset, y_offset) = pred[label_indicies[0], label_indicies[1]][0][1:]
+        if do_scale:
+            x_offset = 2 * (x_offset - 0.5) * OFFSET_WEIGHT
+            y_offset = 2 * (y_offset - 0.5) * OFFSET_WEIGHT
+
+        points[c] = (x_without_offset, y_without_offset, x_offset, y_offset)
+    
+    return points.astype(np.int32)
+
 if __name__ == "__main__":
     print("test")
     input_layer = Input(shape=(200, 200, 3), name="input")
